83e5a7725e42e269cf97e13a0a9276a3b7b8ba54
Ramy Hadid <ramyhadid.js@gmail.com>
2025-05-21 01:08:21 +0100

    Added BookS scraper

--- scrapers/books.go
+++ scrapers/books.go
@@ -4,6 +4,7 @@ import (
 	. "amazon/types"
 	"amazon/utils"
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -27,10 +28,93 @@ func FetchBooks(page int) (*[]BookThumbnail, error) {
 	}
 
 	books := document.Find("[data-asin]")
-	books.Each(func(i int, book *goquery.Selection) {
-		result[i] = BookThumbnail{}
+	if books.Length() == 0 {
+		return nil, utils.Report("Can't find books...")
+	}
 
-	})
+	for i := range books.Length() {
+		bookThumbnail := BookThumbnail{}
+
+		book := books.Eq(i)
+
+		{ // ID
+			bookID, exists := book.Attr("data-asin")
+			if !exists {
+				return nil, utils.Report("Can't find book ID...")
+			}
+			bookThumbnail.ID = bookID
+		}
+
+		{ // Link
+			bookLinkEl := book.Find(".a-link-normal.aok-block")
+			if bookLinkEl.Length() == 0 {
+				return nil, utils.Report("Can't find book element (.a-link-normal.aok-block)")
+			}
+
+			bookLink, exists := bookLinkEl.Attr("href")
+			if !exists {
+				return nil, utils.Report("Can't find book link (.a-link-normal.aok-block)")
+			}
+			bookThumbnail.Link = bookLink
+		}
+
+		{ // Title
+			titleEl := book.Find(".a-link-normal.aok-block span div")
+			if titleEl.Length() == 0 {
+				return nil, utils.Report("Can't find book title...")
+			}
+			bookThumbnail.Title = strings.TrimSpace(titleEl.Text())
+		}
+
+		{ // Cover
+			imageEl := book.Find("img")
+			if imageEl.Length() == 0 {
+				return nil, utils.Report("Can't find book image...")
+			}
+			bookThumbnail.Cover, _ = imageEl.Attr("src")
+		}
+
+		{ // Author
+			authorEl := book.Find(".a-size-small.a-link-child")
+			if authorEl.Length() == 0 {
+				return nil, utils.Report("Can't find book author...")
+			}
+
+			authorNameEl := authorEl.Find("div")
+			if authorNameEl.Length() == 0 {
+				return nil, utils.Report("Can't find book author name...")
+			}
+
+			bookThumbnail.Authors = []AuthorType{
+				{
+					Name:  strings.TrimSpace(authorNameEl.Text()),
+					Link:  authorEl.AttrOr("href", ""),
+					Image: "", // From main page, there is no image available
+				},
+			}
+		}
+
+		{ // Rating
+			ratingEl := book.Find(".a-icon-alt")
+			if ratingEl.Length() == 0 {
+				bookThumbnail.Rating = -1
+				result = append(result, bookThumbnail)
+				continue
+			}
+
+			ratingText := strings.TrimSpace(ratingEl.Text())
+			ratingText = strings.Split(ratingText, "out of")[0]
+			ratingText = strings.ReplaceAll(ratingText, ",", ".")
+			ratingText = strings.TrimSpace(ratingText)
+			rating, err := strconv.ParseFloat(ratingText, 64)
+			if err != nil {
+				return nil, utils.Report("Failed to parse rating value" + err.Error())
+			}
+			bookThumbnail.Rating = float32(rating)
+		}
+
+		result = append(result, bookThumbnail)
+	}
 
 	return &result, nil
 }
