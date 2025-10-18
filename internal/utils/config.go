0dbda33212fa0385b2836962ae0accf498ab8f68
Ramy Hadid <69460661+rhpo@users.noreply.github.com>
2025-10-18 15:01:20 +0100

    Change cache duration from 1 week to 5 years

--- internal/utils/config.go
+++ internal/utils/config.go
@@ -10,7 +10,7 @@ const (
 
 	CACHE_DIRECTORY       = "books_cache"
 	BOOKS_CACHE_DIRECTORY = "books_cache/books"
-	CACHE_DURATION        = 7 * 24 * time.Hour // 1 Week
+	CACHE_DURATION        = 5 * 12 * 30 * 24 * time.Hour // 5 Years
 	MAX_FILE_SIZE         = 25 * 1024 * 1024   // 25 MB
 	TOKEN_EXPIRY          = 3 * 24 * time.Hour // Duration for which the token is valid
 
